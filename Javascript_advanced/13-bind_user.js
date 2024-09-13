43fc9255b0160a83a6b67be412811fa1b186f0fc
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-12 23:41:12 -0400

    Binding + Closure

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,19 @@
+let user = {
+  hobby: "Calligraphy",
+  favoriteSport: "Hockey",
+  astrologicalSign: "Aries",
+  firstName: "Buillaume",
+  lastName: "Johns",
+  location: "Netherlands",
+  occupation: "Engineer",
+};
+
+function logWelcomeUser(welcomeString) {
+  console.log(
+    `${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`
+  );
+}
+
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+bindLogWelcomeUserr("Welcome");
