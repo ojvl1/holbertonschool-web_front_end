30dd6fae662ded28a1ecaf5b5d00ad8235727257
ojvl1 <antompietriosvaldo@gmail.com>
2024-09-12 21:16:42 -0400

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,25 @@
+function countPrimeNumbers() {
+  let count = 0;
+
+  for (let i = 2; i <= 100; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      count++;
+    }
+    console.log(count);
+  }
+}
+for (let k = 0; k <= 100; k++) {
+  countPrimeNumbers();
+}
+let timing = performance.now();
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${timing} milliseconds.`
+);
